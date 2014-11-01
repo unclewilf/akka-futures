cb35d8030cc851e7bd5e836cfe4899b029f0964e
Will Funnell <w.f.funnell@gmail.com>
2014-11-01 13:10:45 +0000

    Service created returning a future

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "akka-future"
+
+version := "1.0"
+
+scalaVersion := "2.11.4"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.3.6",
+  "com.typesafe.akka" %% "akka-testkit" % "2.3.6" % "test",
+  "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test"
+  )
---
+++ src/test/scala/future/HotelServiceTest.scala
@@ -0,0 +1,44 @@
+package future
+
+import org.scalatest.concurrent.{AsyncAssertions, ScalaFutures}
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.concurrent.ExecutionContext.Implicits._
+import scala.concurrent.Future
+import scala.util._
+
+class HotelServiceTest extends FlatSpec with Matchers with ScalaFutures with AsyncAssertions {
+
+  val service = new HotelService
+
+  it should "return a valid id for even number" in {
+
+    val futureRes: Future[String] = service.find(2)
+
+    whenReady(futureRes) {
+      res => res should equal("2")
+    }
+  }
+
+  it should "return a valid id for even number again" in {
+
+    val futureRes: Future[String] = service.find(4)
+
+    futureRes.futureValue should equal("4")
+  }
+
+  it should "throw for odd number" in {
+
+    val futureRes: Future[String] = service.find(1)
+    val w = new Waiter
+
+    futureRes onComplete {
+      case Failure(e) => w(throw e); w.dismiss()
+      case Success(_) => w.dismiss()
+    }
+
+    intercept[IllegalArgumentException] {
+      w.await
+    }
+  }
+}
