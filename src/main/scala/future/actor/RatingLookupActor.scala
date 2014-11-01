8c50a8d6b8e71b203f426805d801520486dc4a2b
Will Funnell <w.f.funnell@gmail.com>
2014-11-01 23:48:43 +0000

    Added rating lookup actor

---
+++ src/main/scala/future/actor/RatingLookupActor.scala
@@ -0,0 +1,27 @@
+package future.actor
+
+import akka.actor.{Actor, Props}
+import akka.pattern.pipe
+import future.domain.Rating
+import future.service.RatingService
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+object RatingLookupActor {
+  def props(service: RatingService): Props = Props(new RatingLookupActor(service))
+}
+
+class RatingLookupActor(service: RatingService) extends Actor {
+
+  override def receive: Receive = {
+    case id: Int => respondToSender(id)
+  }
+
+  def respondToSender(i: Int): Unit = {
+
+    val hotel: Future[Rating] = service.find(i)
+
+    pipe(hotel) to sender
+  }
+}
